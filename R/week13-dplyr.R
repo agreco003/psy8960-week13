69dca36ec20c0ced6463b3f391814dfc3a8b2e55
agreco <greco031@umn.edu>
2023-04-28 14:17:57 -0500

    deleted final line

--- R/week13-dplyr.R
+++ R/week13-dplyr.R
@@ -1,3 +1,31 @@
+# Script Settings and Resources
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
-library(tidyverse)
-week13_tbl <- read_csv(file ="../data/week13.csv")
+library(dplyr)
+
+# Data Import and Cleaning
+week13_tbl <- tibble(read.csv(file ="../data/week13.csv"))
+
+# Analysis - dplyr
+## Count of managers
+count(week13_tbl)
+
+## Count of distinct managers
+distinct(week13_tbl, pick(employee_id)) %>%
+  count()
+
+## Number of managers by location, not originally hired as managers
+group_by(week13_tbl, city) %>%
+  filter(manager_hire == "N") %>%
+  count(city)
+
+## Avg years employeed, sd of years employed by performance group
+group_by(week13_tbl, performance_group) %>%
+  summarize(avg = mean(yrs_employed),
+            sd = sd(yrs_employed))
+
+## Top 3 managers by location, sorted first by city and then test score. Ties included
+group_by(week13_tbl, city) %>%
+  slice_max(n = 3, 
+            order_by = tibble(city, test_score),
+            with_ties = TRUE) %>%
+  select(employee_id, city) #add test_score to verify test_score descending order
